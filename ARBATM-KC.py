8832b4a89b7e98ef8275e315799eda8acd5006c6
wkhuen <60916798+wkhuen@users.noreply.github.com>
2022-01-14 08:56:23 +0800

    Update ARBATM-KC.py

--- ARBATM-KC.py
+++ ARBATM-KC.py
@@ -15,12 +15,32 @@ import csv
 import keyboard #pip install keyboard
 from datetime import datetime
 import time as t
+#import gsheet
 
 ########################################################################
 import API_Keys # from API_Keys import KC_SPOT
-client = Market_Spot(API_Keys.KC_SPOT['key'], API_Keys.KC_SPOT['secret'], API_Keys.KC_SPOT['passphrase'], API_Keys.KC_SPOT['is_sandbox'])
-client_f = Market_Futures(API_Keys.KC_FUTURES['key'], API_Keys.KC_FUTURES['secret'], API_Keys.KC_FUTURES['passphrase'], API_Keys.KC_FUTURES['is_sandbox'])
+client_sm = Market_Spot(API_Keys.KC_SPOT['key'], API_Keys.KC_SPOT['secret'], API_Keys.KC_SPOT['passphrase'], API_Keys.KC_SPOT['is_sandbox'])
+client_st = Trade_Spot(API_Keys.KC_SPOT['key'], API_Keys.KC_SPOT['secret'], API_Keys.KC_SPOT['passphrase'], API_Keys.KC_SPOT['is_sandbox'])
+client_fm = Market_Futures(API_Keys.KC_FUTURES['key'], API_Keys.KC_FUTURES['secret'], API_Keys.KC_FUTURES['passphrase'], API_Keys.KC_FUTURES['is_sandbox'])
 client_ft = Trade_Futures(API_Keys.KC_FUTURES['key'], API_Keys.KC_FUTURES['secret'], API_Keys.KC_FUTURES['passphrase'], API_Keys.KC_FUTURES['is_sandbox'])
+"""
+client_sm = Market_Spot(API_Keys.KC_SPOT_SANDBOX['key'],
+                        API_Keys.KC_SPOT_SANDBOX['secret'],
+                        API_Keys.KC_SPOT_SANDBOX['passphrase'],
+                        API_Keys.KC_SPOT_SANDBOX['is_sandbox'])
+client_st = Trade_Spot(API_Keys.KC_SPOT_SANDBOX['key'],
+                       API_Keys.KC_SPOT_SANDBOX['secret'],
+                       API_Keys.KC_SPOT_SANDBOX['passphrase'],
+                       API_Keys.KC_SPOT_SANDBOX['is_sandbox'])
+client_fm = Market_Futures(API_Keys.KC_FUTURES_SANDBOX['key'],
+                           API_Keys.KC_FUTURES_SANDBOX['secret'],
+                           API_Keys.KC_FUTURES_SANDBOX['passphrase'],
+                           API_Keys.KC_FUTURES_SANDBOX['is_sandbox'])
+client_ft = Trade_Futures(API_Keys.KC_FUTURES_SANDBOX['key'],
+                          API_Keys.KC_FUTURES_SANDBOX['secret'],
+                          API_Keys.KC_FUTURES_SANDBOX['passphrase'],
+                          API_Keys.KC_FUTURES_SANDBOX['is_sandbox'])
+"""
 ########################################################################
 
 ########################################################################
@@ -53,99 +73,81 @@ api_retry_max = 5
 #HTTP:
 #https://api-futures.kucoin.com/api/v1/ticker?symbol=XBTUSDM
 #https://api-futures.kucoin.com/api/v1/level2/depth100?symbol=XBTUSDM
+#https://api.kucoin.com/api/v1/symbols
+#https://api-futures.kucoin.com/api/v1/contracts/active
 
-def proximity_in_orderbook(sym, sym_f, buy_client, buy_price, max_diff_buy, sell_client, sell_price, max_diff_sell, size):
-    diff_buy = 0
-    diff_sell = 0
-    asks_size_accum = float(0)
-    asks_index = 0
-    bids_size_accum = float(0)
-    bids_index = 0
+#global ob_buy
+
+def proximity_in_orderbook(sym, sym_f,
+                           buy_client, tgt_buy_price, max_diff_buy,
+                           sell_client, tgt_sell_price, max_diff_sell,
+                           size, mode):
+    # Mode 1: Buy S, Sell F --> Hedge, Add
+    # Mode 2: Sell S, Buy F --> Close All
+    asks_size_accum, asks_price_accum, diff_buy_pct, asks_index = 0, 0, 0, 0
+    bids_size_accum, bids_price_accum, diff_sell_pct, bids_index = 0, 0, 0, 0
+    ask_buy_ok, bid_sell_ok = False, False
     proximity_in_orderbook_txt = ""
     proximity_short_txt = ""
 
     api_retry_count = 0
-    while api_retry_count <= api_retry_max:
+    while api_retry_count < api_retry_max:
         try:
-            print("Before " + sym + sym+sym+sym+sym)
-            #ob_buy = buy_client.l2_order_book(symbol=sym)    # Use Buy-Side Client
-            ob_buy = buy_client.get_aggregated_orderv3(symbol=sym)
-            print("After " + sym + sym + sym + sym + sym)
-            #print("OBBuy", ob_buy)
-
-            #https: // api - futures.kucoin.com / api / v1 / level2 / depth100?symbol = XBTUSDM
-            ob_sell = sell_client.l2_order_book(symbol=sym_f)  # Use Sell-Side Client
-            #ob_sell = sell_client.get_order_book(symbol=sym_f)  # Use Sell-Side Client
-            #print("OBSell", ob_sell)
+            if mode == 1:
+                #Mode 1: Buy S, Sell F --> Hedge, Add
+                ob_buy = buy_client.get_aggregated_orderv3(symbol=sym)  # Buy S
+                ob_sell = sell_client.l2_order_book(symbol=sym_f)       # Sell F
+            else:
+                #Mode 2: Sell S, Buy F --> Close All
+                ob_buy = buy_client.l2_order_book(symbol=sym_f)               # Buy F
+                ob_sell = sell_client.get_aggregated_orderv3(symbol=sym)  # Sell S
             break
         except:
-            t.sleep(1)
+            t.sleep(2)
             api_retry_count += 1
             print("ERROR (2)!!! when <proximity_in_orderbook:get_order_book>  " + " Attempt: " + str(api_retry_count))
             pass
 
-    #Check Buy-Side (Ask) Proximity (-ve Good, +ve NOT Good)
-    #for i in range(num_asks_comp):
-    #    diff = (float(ob_buy['asks'][i-1][0]) - buy_price) / buy_price * 100
-    #    proximity_in_orderbook_txt = proximity_in_orderbook_txt + "DesiredBuyPrice: " + str(buy_price) + " asks[" + str(i) + "]:" + ob_buy['asks'][i][0] + " " + str(diff) + "%" + "\r\n"
-    #    if float(diff_buy) < float(diff):
-    #        diff_buy = diff
-
-    #Check Sell-Side (Bid) Proximity (-ve Good, +ve NOT Good)
-    #for j in range(num_bids_comp):
-    #    diff = (sell_price - float(ob_sell['bids'][j-1][0])) / sell_price * 100
-    #    proximity_in_orderbook_txt = proximity_in_orderbook_txt + "DesiredSellPrice: " + str(sell_price) + " bids[" + str(j) + "]: " + ob_sell['bids'][j][0] + " " + str(diff) + "%" + "\r\n"
-    #    if float(diff_sell) < float(diff):
-    #        diff_sell = diff
-
     while True:
         asks_size_accum = asks_size_accum + float(ob_buy['asks'][asks_index][1])
-        diff_buy = diff_buy + (float(ob_buy['asks'][asks_index][0]) * float(ob_buy['asks'][asks_index][1]))
-        #print(str(asks_index) + ": " +
-        #      "desired " + str(buy_price) + "/" + str(size) + " " +
-        #      "ask " + str(ob_buy['asks'][asks_index][0]) + " " +
-        #      "size " + str(ob_buy['asks'][asks_index][1]) + " " +
-        #      "ttl ask " + str(diff_buy) + " " +
-        #      "ttl size " + str(asks_size_accum) + " " +
-        #      "AVG: " + str(float(diff_buy) / float(asks_size_accum)))
+        asks_price_accum = asks_price_accum + (float(ob_buy['asks'][asks_index][0]) * float(ob_buy['asks'][asks_index][1])) # [0]=Price, [1]=Size
         if asks_size_accum > size:
-            #diff_buy = (float(ob_buy['asks'][asks_index][0]) - buy_price) / buy_price * 100
-            diff_buy = ((float(diff_buy)/asks_size_accum) - buy_price) / buy_price * 100
+            diff_buy_pct = ((asks_price_accum / asks_size_accum) - tgt_buy_price) / tgt_buy_price * 100 #-ve Good, +ve NOT Good
             #print ("diff_buy:" + str(diff_buy))
-            proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Buy / Asks, " + "DesiredBuyPrice: " + str(buy_price) + "\r\n"
+            proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Buy / Asks, " + "DesiredBuyPrice: " + str(tgt_buy_price) + "\r\n"
             proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Takes " + str(asks_index + 1) + " entries to reach " + str(size) + " " + sym + "\r\n"
-            proximity_in_orderbook_txt = proximity_in_orderbook_txt + str(asks_index + 1) + "-th <ask> price = " + str(ob_buy['asks'][asks_index][0]) + ", diff = " + formatter_5d_pn.format(diff_buy) + "%" + "\r\n" + "\r\n"
+            proximity_in_orderbook_txt = proximity_in_orderbook_txt + str(asks_index + 1) + "-th <ask> price = " + str(ob_buy['asks'][asks_index][0]) + ", diff = " + formatter_5d_pn.format(diff_buy_pct) + "%" + "\r\n" + "\r\n"
             break
         asks_index += 1
 
     while True:
         bids_size_accum = bids_size_accum + float(ob_sell['bids'][bids_index][1])
-        #diff_buy = diff_buy + (float(ob_buy['asks'][asks_index][0]) * float(ob_buy['asks'][asks_index][1]))
-        diff_sell = diff_sell + (float(ob_sell['bids'][bids_index][0]) * float(ob_sell['bids'][bids_index][1]))
+        bids_price_accum = bids_price_accum + (float(ob_sell['bids'][bids_index][0]) * float(ob_sell['bids'][bids_index][1])) # [0]=Price, [1]=Size
         if bids_size_accum > size:
-            #diff_sell = (sell_price - float(ob_sell['bids'][bids_index][0])) / sell_price * 100
-            diff_sell = ((float(diff_sell) / bids_size_accum) - sell_price) / sell_price * 100
+            diff_sell_pct = (tgt_sell_price - (bids_price_accum / bids_size_accum)) / tgt_sell_price * 100 #-ve Good, +ve NOT Good
             #print("diff_sell:" + str(diff_sell))
-            proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Sell / Bids, " + "DesiredSellPrice: " + str(sell_price) + "\r\n"
+            proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Sell / Bids, " + "DesiredSellPrice: " + str(tgt_sell_price) + "\r\n"
             proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Takes " + str(bids_index + 1) + " entries to reach " + str(size) + " " + sym + "\r\n"
-            proximity_in_orderbook_txt = proximity_in_orderbook_txt + str(bids_index + 1) + "-th <bid> price = " + str(ob_sell['bids'][bids_index][0]) + ", diff = " + formatter_5d_pn.format(diff_sell) + "%" + "\r\n" + "\r\n"
+            proximity_in_orderbook_txt = proximity_in_orderbook_txt + str(bids_index + 1) + "-th <bid> price = " + str(ob_sell['bids'][bids_index][0]) + ", diff = " + formatter_5d_pn.format(diff_sell_pct) + "%" + "\r\n" + "\r\n"
             break
         bids_index += 1
 
-    #print ("asks_index: ", str(asks_index), "size: ", asks_size_accum)
-    #print ("bids_index: ", str(bids_index), "size: ", bids_size_accum)
-
-    if diff_buy <= max_diff_buy:
-        proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Buy / Asks: " + formatter_5d_pn.format(diff_buy) + "% within " + str(max_diff_buy) + " limit " + "\r\n"
+    ask_buy_ok = False
+    if diff_buy_pct <= max_diff_buy:
+        ask_buy_ok = True
+        proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Buy / Asks: " + formatter_5d_pn.format(diff_buy_pct) + "% within " + str(max_diff_buy) + " limit " + "\r\n"
     else:
-        proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Buy / Asks: " + formatter_5d_pn.format(diff_buy) + "% exceeded " + str(max_diff_buy) + " limit " + "\r\n"
+        proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Buy / Asks: " + formatter_5d_pn.format(diff_buy_pct) + "% exceeded " + str(max_diff_buy) + " limit " + "\r\n"
 
-    if diff_sell <= max_diff_sell:
-        proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Sell / Bids: " + formatter_5d_pn.format(diff_sell) + "% within " + str(max_diff_sell) + " limit " + "\r\n"
+    bid_sell_ok = False
+    if diff_sell_pct <= max_diff_sell:
+        bid_sell_ok = True
+        proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Sell / Bids: " + formatter_5d_pn.format(diff_sell_pct) + "% within " + str(max_diff_sell) + " limit " + "\r\n"
     else:
-        proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Sell / Bids: " + formatter_5d_pn.format(diff_sell) + "% exceeded " + str(max_diff_sell) + " limit " + "\r\n"
+        proximity_in_orderbook_txt = proximity_in_orderbook_txt + "Sell / Bids: " + formatter_5d_pn.format(diff_sell_pct) + "% exceeded " + str(max_diff_sell) + " limit " + "\r\n"
 
-    if diff_buy <= max_diff_buy and diff_sell <= max_diff_sell:
+    #if diff_buy_pct <= max_diff_buy and diff_sell_pct <= max_diff_sell:
+    if ask_buy_ok and bid_sell_ok:
         proximity_in_orderbook_txt = proximity_in_orderbook_txt + "<<Within Limit -- Proceed to order>>"
         proximity_short_txt = "Within Limit: OK To Proceed"
         return True, proximity_in_orderbook_txt, proximity_short_txt  #OK to Prceed
@@ -210,32 +212,21 @@ def place_market_order (sym, buy_client, buy_price, sell_client, sell_price, lot
 sym = "LUNA-USDT"
 sym_f = "LUNAUSDTM"
 lotsize = 1
+
+i = 2
+delta, delta_pct = 0, 0
+num_pos, max_pos, num_cycle = 0, 3, 0
+hedge_bottom, hedge_top = -0.1, 0.1
+range_bottom, range_top = -0.15, 0.15
+
+action_rmk = ""
+ob_text = ""
 f = open(sym+".csv", "a", newline="")
 f.truncate(0)
 writer = csv.writer(f)
-tup1 = ("Index", "Local-Time", "S_TimeStamp", "S_Price$", "F_TimeStamp", "F_Price$", "F-S_TimeStamp", "F-S_Price$", "(F-S)/S_Price%", "Action", "Num_Pos", "OrderBook")
+tup1 = ("Index", "Local-Time", "S_TimeStamp", "S_Price$", "F_TimeStamp", "F_Price$", "F-S_TimeStamp", "F-S_Price$", "(F-S)/S_Price%", "Action", "Num_Cycle", "Num_Pos", "OrderBook")
 writer.writerow(tup1)
-i = 2
-delta = 0
-#last_delta = 0
-delta_pct = 0
-max_pos = 3
-num_pos = 0
-action_rmk = ""
-ob_text = ""
-hedge_top = 0.05
-hedge_bottom = -0.05
-range_top = 0.1
-range_bottom = -0.1
 
-
-#print(sym)
-#print(client.get_ticker(sym))
-#l2_depth = client_f.l2_order_book('XBTUSDM')
-#print(sym_f)
-#print ("123123" , client_f.l2_order_book(sym_f))
-#print("@@@@", client_f.get_ticker(sym_f))
-#id_sell = client_f.create_market_order(sym, client.SIDE_SELL, size=1)
 #sys.exit()
 
 
@@ -243,23 +234,20 @@ while True:
     t.sleep(2)
     local_dt = datetime.fromtimestamp(int(t.time()))
 
-    #spot = client.get_ticker(sym)  # float(client.get_ticker(sym)['price'])
-    #spot_price = float(spot['price'])
-    #print(spot_price)
-
     #ob_buy = buy_client.l2_order_book(symbol=sym)
     #bcsa = client.get_aggregated_order(symbol=sym) #get_aggregated_order(symbol=sym) RETIRED ??
     #print (bcsa)
 
     print("=" * 156)
+    print("Iteration: " + str(i) + " "*3 + "Spot: " + sym + ", Futures: " + sym_f)
     api_retry_count = 0
     while api_retry_count <= api_retry_max:
         try:
-            spot = client.get_ticker(sym) #float(client.get_ticker(sym)['price'])
+            spot = client_sm.get_ticker(sym) #float(client.get_ticker(sym)['price'])
             spot_price = float(spot['price'])
             spot_dt = datetime.fromtimestamp(int(spot['time']) / 1000)     # using the local timezone
             #print("Spot:", spot)
-            future = client_f.get_ticker(sym_f)
+            future = client_fm.get_ticker(sym_f)
             #print("Future:", future)
             future_price = float(future['price'])
             #future_dt = datetime.fromtimestamp(int(future['sequence']) / 1000)  # using the local timezone
@@ -282,7 +270,8 @@ while True:
     if num_pos == 0:
         if delta_pct >= hedge_bottom and delta_pct <= hedge_top: #Very Difficult to be 0
             #Check next 2 bid/ask in OrderBook
-            proceed = proximity_in_orderbook(sym, sym_f, client, spot_price, 0.01, client_f, future_price, 0.01, lotsize)
+            print('Open Hedge')
+            proceed = proximity_in_orderbook(sym, sym_f, client_sm, spot_price, 0.01, client_fm, future_price, 0.01, lotsize, mode=1)
             ob_text = proceed[1]
             proximity_text = proceed[2]
             if(proceed[0] == True):
@@ -296,7 +285,8 @@ while True:
     else:
         if delta_pct >= range_top:
             if num_pos < max_pos:
-                proceed = proximity_in_orderbook(sym, sym_f, client, spot_price, 0.01, client_f, future_price, 0.01, lotsize)
+                proceed = proximity_in_orderbook(sym, sym_f, client_sm, spot_price, 0.01, client_fm, future_price, 0.01, lotsize, mode=1)
+                #proceed = proximity_in_orderbook(sym, sym_f, client_sm, spot_price, 0.01, client_fm, future_price, 0.01, lotsize)
                 ob_text = proceed[1]
                 proximity_text = proceed[2]
                 if (proceed[0] == True):
@@ -308,13 +298,14 @@ while True:
             else:
                 action_rmk = "Should Add Position (Buy S, Sell F), but already hit max "
         if delta_pct <= range_bottom:
-            proceed = proximity_in_orderbook(sym, sym_f, client_f, future_price, 0.01, client, spot_price, 0.01, lotsize)
+            proceed = proximity_in_orderbook(sym, sym_f, client_fm, future_price, 0.01, client_sm, spot_price, 0.01, lotsize*num_pos, mode=2)
             ob_text = proceed[1]
             proximity_text = proceed[2]
             if (proceed[0] == True):
                 #Close All Positions
                 action_rmk = "Close All (Sell S, Buy F)  " + str(num_pos) + " Positions"
                 num_pos = 0
+                num_cycle += 1
             else:
                 action_rmk = "Close All (Sell S, Buy F) <Halted>, OrderBook exceeded Limit "
 
@@ -325,11 +316,14 @@ while True:
            " <Time (F-S)>: " + str(formatter_5d_pn.format((future_dt - spot_dt).total_seconds())) + "\n" + \
            bcolors.OKBLUE + "<Delta>: " + str(formatter_5d_pn.format(delta)) + bcolors.NORMAL + \
            bcolors.OKCYAN + " <Delta %>: " + str(formatter_5d_pn.format(delta_pct)) + "%" + bcolors.NORMAL + \
-           " <Action>: " + "{:<98}".format(action_rmk) + \
-           " NumPos:" + str(num_pos) + "\n" + \
+           " <Action>: " + "{:<84}".format(action_rmk) + \
+           " NumCycle:" + str(num_cycle).zfill(2) + "  NumPos:" + str(num_pos).zfill(2) + "\n" + \
            bcolors.WARNING + bcolors.BOLD + "<Proximity Check>: " + str(proximity_text) + bcolors.NORMAL + "\n" + \
            bcolors.WARNING + bcolors.BOLD + "<Order Action>: " + str(order_text) + bcolors.NORMAL
-    tup1 = (str(i), str(local_dt), spot_dt.strftime("%Y-%m-%d %H:%M:%S.%f"), str(spot_price), future_dt.strftime("%Y-%m-%d %H:%M:%S.%f"), str(future_price), (future_dt - spot_dt).total_seconds(), str("{:.5f}".format(delta)) , str("{:.5f}".format(delta_pct)), action_rmk, num_pos, ob_text)
+    tup1 = (str(i), str(local_dt),
+            spot_dt.strftime("%Y-%m-%d %H:%M:%S.%f"),
+            str(spot_price), future_dt.strftime("%Y-%m-%d %H:%M:%S.%f"),
+            str(future_price), (future_dt - spot_dt).total_seconds(), str("{:.5f}".format(delta)), str("{:.5f}".format(delta_pct)), action_rmk, num_cycle, num_pos, ob_text)
     writer.writerow(tup1)
     print(disp)
 
@@ -339,4 +333,6 @@ while True:
         print ("q pressed, end loop")
         break
 f.close()
+
+
 
