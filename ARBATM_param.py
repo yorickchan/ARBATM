a506870fc09fb1a24b2a94fc5e7fc2c75397f1f2
shrimpfu <60916798+wkhuen@users.noreply.github.com>
2022-02-19 17:13:10 +0800

    Update ARBATM_param.py

--- ARBATM_param.py
+++ ARBATM_param.py
@@ -15,7 +15,7 @@ range_bottom, subtract_top, hedge_bottom, range_top = -0.32, -0.16, 0.16, 0.32
 #range_bottom, range_top = -0.4, 0.4 #-0.25, 0.25   # in %
 #subtract_top = -0.16                # 0.16% round-trip-fees
 
-#ob_var_allow_pct = 0.03         # 0.01 # in % -- Use <Delta_Pct> instead
+#ob_var_allow_pct = 0.03        # 0.01 # in % -- Use <Delta_Pct> instead
 ts_var_allow_sec = 1.5          # ticker timestamp of S/F in seconds 0.5
 ts_order_fill_gap = 10          # wait time(sec) between Order / Review, 5 sec is too early to review
 timeout_to_close = 3600         #in seconds
@@ -27,7 +27,7 @@ spot_fee_pct = 0.1      #0.1%
 futures_fee_pct = 0.06  #vary from 0.03% to 0%
 
 beep = True
-output_to_terminal = 2      #0=Nothing, 1=Attempted and Order Only, 2=All
+output_to_terminal = 2     #0=Nothing, 1=Attempted and Order Only, 2=All
 output_to_csv = True       # Only Write orderId
 output_to_gsheet = True    # Can run faster if False
 
